acd7acbc2f8b30c670908bc4cba6fb7731ec563b
Jackie <grecoj2@hawkmail.newpaltz.edu>
2017-03-27 10:43:46 -0400

    Added Types and Player Code

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int health = 100;
+
+    new Rigidbody2D rigidbody; 
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        rigidbody.velocity = new Vector2(x, 0);
+	
+	}
+}
